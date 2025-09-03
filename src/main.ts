4cb263399a646b2c3208a841ab286c36cb1a51dc
bernardopadilha <bernardo_limas@hotmail.com>
2025-09-03 10:32:26 -0300

    chore: configurado projeto NestJS com configurações básicas

---
+++ src/main.ts
@@ -0,0 +1,50 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+import { apiReference } from '@scalar/nestjs-api-reference';
+
+const PORT = Number(process.env.PORT || 3000);
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin:
+      process.env.NODE_ENV === 'development'
+        ? ['http://localhost:5173', 'http://127.0.0.1:5173']
+        : [/\.bernardopadilha\.com\.br$/],
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('ControlAI Docs')
+    .setDescription('Desafio para vaga front-end')
+    .setVersion('1.0.0')
+    .setContact(
+      'Bernardo Padilha',
+      'https://www.bernardopadilha.com.br/',
+      'bernardoa.padilha@gmailcom',
+    )
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+    })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  app.use(
+    '/docs',
+
+    apiReference({
+      content: document,
+    }),
+  );
+
+  await app.listen(PORT, () => {
+    console.log(`🦁 NestJS listening at http://localhost:${PORT}/`);
+    console.log(`📚 Scalar docs at http://localhost:${PORT}/docs`);
+  });
+}
+bootstrap();
