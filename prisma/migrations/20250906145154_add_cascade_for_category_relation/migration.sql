e6fa1ac42d56c1e133d76f171795a0c71f061f68
bernardopadilha <bernardo_limas@hotmail.com>
2025-09-06 12:10:49 -0300

    feat: adicionado cascade para relação de expense e category

---
+++ prisma/migrations/20250906145154_add_cascade_for_category_relation/migration.sql
@@ -0,0 +1,20 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Expense" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "amount" INTEGER NOT NULL,
+    "description" TEXT NOT NULL,
+    "date" DATETIME NOT NULL,
+    "userId" TEXT NOT NULL,
+    "categoryId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Expense_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Expense_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Expense" ("amount", "categoryId", "createdAt", "date", "description", "id", "updatedAt", "userId") SELECT "amount", "categoryId", "createdAt", "date", "description", "id", "updatedAt", "userId" FROM "Expense";
+DROP TABLE "Expense";
+ALTER TABLE "new_Expense" RENAME TO "Expense";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
